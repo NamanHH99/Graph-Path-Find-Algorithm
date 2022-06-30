523e4bc6ee96e4c25b2f11c18412f9df462769ec
NamanHH99 <namanhemrajani@gmail.com>
2022-07-01 01:26:43 +0530

    add all files

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateGrids from "./CreateGrids";
+function App() {
+  return (
+    <div>
+      <div  className="heading">
+        <h1>Path Finding Algorithms</h1>
+      </div>
+      <CreateGrids />
+      <div className="copyright"><h6>© Namarth 2022</h6></div>
+    </div>
+  );
+}
+
+export default App;
