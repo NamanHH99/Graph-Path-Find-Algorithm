c7f1742b915df0729e4a03dd9010ce5afb69f90a
NamanHH99 <namanhemrajani@gmail.com>
2022-07-01 09:39:58 +0530

    remove help due to some error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <div  className="heading">
         <h1>Path Finding Algorithms</h1>
-        <button className = "btn reset btn-lg btn-info" >Help</button>
+        {/* <button className = "btn reset btn-lg btn-info" >Help</button> */}
       </div>
       <CreateGrids />
       <div className="copyright"><h6>© Namarth 2022</h6></div>
